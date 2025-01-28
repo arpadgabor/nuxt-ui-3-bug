62076eaa83a070f4f6ad01a21c761566ba4270d0
Arpad Gabor <hello@arpadgabor.com>
2025-01-28 20:51:05 +0200

    simplify

--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,32 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <USlideover title="Slideovers">
-    <UButton>Open</UButton>
-
-    <template #body>
-      <Form class="w-full flex items-center justify-center h-full">
-        <Field name="field" v-slot="{ componentField }">
-          <USelectMenu class="w-32" placeholder="Select item" :items="['Item A', 'Item B']" v-bind="componentField" />
-        </Field>
-        <Field name="field_2" v-slot="{ componentField }">
-          <USelectMenu class="w-32" placeholder="Select item" :items="['Item A', 'Item B']" v-bind="componentField" />
-        </Field>
-        <Field name="field_3" v-slot="{ componentField }">
-          <USelect class="w-32" placeholder="Select item" :items="['Item A', 'Item B']" v-bind="componentField" />
-        </Field>
-      </Form>
-    </template>
-  </USlideover>
+  <div>
+    <Form class="w-full flex items-center justify-center h-full">
+      <Field name="field" v-slot="{ componentField }">
+        <USelectMenu
+          class="w-32"
+          placeholder="Select item"
+          :items="['Item A', 'Item B']"
+          v-bind="componentField"
+        />
+      </Field>
+      <Field name="field_2" v-slot="{ componentField }">
+        <USelectMenu
+          class="w-32"
+          placeholder="Select item"
+          :items="['Item A', 'Item B']"
+          v-bind="componentField"
+        />
+      </Field>
+      <Field name="field_3" v-slot="{ componentField }">
+        <USelect
+          class="w-32"
+          placeholder="Select item"
+          :items="['Item A', 'Item B']"
+          v-bind="componentField"
+        />
+      </Field>
+    </Form>
+  </div>
 </template>
